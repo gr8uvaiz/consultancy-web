f3cce3755867d08cfa49186ecd46cd21ad4e0fc9
gr8uvaiz <mohammeduvaiz0786@gmail.com>
2023-10-02 00:38:15 +0530

    Design About Page

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,5 +1,10 @@
 module.exports.home = function(req,res){
     res.render('home',{
-        title: 'Home',
+        title: 'Home | Consult Pro',
+    })
+}
+module.exports.about = function(req,res){
+    res.render('about',{
+        title: "About | Consult Pro"
     })
 }
--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const homeController = require('../controllers/home_controller')
 
 router.get('/',homeController.home);
+router.get('/about',homeController.about)
 router.use('/users',require('./user'));
 router.use('/services',require('./service'));
 
